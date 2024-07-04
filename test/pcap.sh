4de446ff3a6a7046b35f0f3368269c2d01458d22
Looby72 <looby1199@gmail.com>
2024-07-05 00:26:15 +0200

    test container automatically downloads example file

--- test/pcap.sh
+++ test/pcap.sh
@@ -3,5 +3,15 @@
 # create a fifo pipe filled with test data
 rm -f pcap/*
 mkfifo pcap/testFifo
+
+FILE="test_data.pcap"
+if [ ! -f "$FILE" ]; then
+    # download test pcap data
+    echo "Downloading $FILE ..."
+    wget -q -O "${FILE}.gz" "https://share.netresec.com/s/wC4mqF2HNso4Ten/download?path=%2F&files=maccdc2010_00000_20100310205651.pcap.gz&downloadStartSecret=fprppure7s"
+    gunzip "${FILE}.gz"
+else
+    echo "File $FILE already exists."
+fi
 echo "creating test pipe"
-cat testData.pcap > pcap/testFifo
+cat test_data.pcap > pcap/testFifo
